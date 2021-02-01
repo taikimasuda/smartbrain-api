ed564d3edc8d2f2039885ce2b0ae9846bbcacb0d
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-02-02 01:19:29 +0900

    changing response

--- server.js
+++ server.js
@@ -23,11 +23,7 @@ const db = knex({
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res)=> {
-  res.send(database.users);
-})
-
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
