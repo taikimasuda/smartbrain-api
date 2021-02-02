19b2dbb9cd3447615b8b18583b36255d124cb5e8
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-02-02 09:35:17 +0900

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-trapezoidal-19858',
     user : 'taikimasuda',
     password : '',
     database : 'smart-brain'
