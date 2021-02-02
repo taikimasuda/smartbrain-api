7f81b4eb7b6c5c22f852bcd0e7ad087602eb82c8
Taiki Masuda <t2gucqkc18863j73j2eb@yahoo.co.jp>
2021-02-02 09:45:37 +0900

    adding database connection

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
   connection: {
-    host : 'postgresql-trapezoidal-19858',
-    user : 'taikimasuda',
-    password : '',
-    database : 'smart-brain'
+    host : 'process.env.DATABASE_URL',
+  	ssl: {
+    	rejectUnauthorized: false
+  	}
   }
 });
 
